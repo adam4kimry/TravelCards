09a7ca6c39ca6689b566a3ea27570f2b1752e02f
adam4kimry <adam4kimry@gmail.com>
2016-11-06 13:33:15 +0300

    create

---
+++ TestTravelCards/TestTravelCards.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TravelCards;
+
+namespace TestTravelCards
+{
+    [TestClass]
+    public class TestTravelCards
+    {
+        [TestMethod]
+        public void TestInvalidArgs()
+        {
+            TravelCard card1;
+            card1.CityFrom = "Мельбурн";
+            card1.CityTo = "Кельн";
+            TravelCard card2;
+            card2.CityFrom = "Москва";
+            card2.CityTo = "Париж";
+            TravelCard card3;
+            card3.CityFrom = "Кельн";
+            card3.CityTo = "Москва";
+            TravelCard card4;
+            card4.CityFrom = "Рига";
+            card4.CityTo = "Москва";
+            TravelCard[] cards = { card1, card2, card3, card4 };
+            TravelHelper.Sort(cards);
+        }
+
+        [TestMethod]
+        public void TestSampleArgs()
+        {
+            TravelCard card1;
+            card1.CityFrom = "Мельбурн";
+            card1.CityTo = "Кельн";
+            TravelCard card2;
+            card2.CityFrom = "Москва";
+            card2.CityTo = "Париж";
+            TravelCard card3;
+            card3.CityFrom = "Кельн";
+            card3.CityTo = "Москва";
+            TravelCard[] cards = { card1, card2, card3 };
+            TravelHelper.Sort(cards);
+        }
+    }
+}
---
+++ TravelCards/TravelHelper.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TravelCards
+{
+    public static class TravelHelper
+    {
+        //сложность 3N + N^2 / 2 -> N^2 / 2
+        public static TravelCard[] Sort(TravelCard[] cards)
+        {
+            //проверка на вилидность входных данных
+            if (cards == null) return null;
+            if (cards.Length == 0) return cards;
+
+            var citiesFrom = new List<string>();
+            var citiesTo = new List<string>();
+
+            foreach (var card in cards)
+            {
+                //если есть повторное вхождение, значит на лицо зацикленность/разветвление, что не допускается в условии
+                if (citiesFrom.Contains(card.CityFrom) || citiesTo.Contains(card.CityTo))
+                    throw new Exception("Invalid input data [D32E68BC02EC]");
+
+                citiesFrom.Add(card.CityFrom);
+                citiesTo.Add(card.CityTo);
+            }
+
+            var startCities = citiesFrom.Where(x => !citiesTo.Contains(x)).ToList();
+            var endCities = citiesTo.Where(x => !citiesFrom.Contains(x)).ToList();
+
+            //если количество пунктов отправления, которые не фигурируют в качестве пунктов назначения в других картах, больше 1 или отсутствуют
+            //или же количество пунктов назначения, которые не фигурируют в качестве пунктов отправления в других картах, больше 1 или отсутствуют
+            if (startCities.Count != 1 || endCities.Count != 1)
+                throw new Exception("Invalid input data [532FBB0A1DE2]");
+
+            //изначальный пункт отправления и конечный пункт назначения
+            var startCity = startCities[0];
+            var endCity = endCities[0];
+
+            var k = 0;
+
+            while (startCity != endCity)
+            {
+                for (var i = k; i < cards.Length; i++)
+                {
+                    if (cards[i].CityFrom == startCity)
+                    {
+                        startCity = cards[i].CityTo;
+                        var card = cards[i];
+                        cards[i] = cards[k];
+                        cards[k] = card;
+                        k++;
+                        break;
+                    }
+                }
+            }
+
+            return cards;
+        }
+    }
+
+    public struct TravelCard
+    {
+        public string CityFrom;
+        public string CityTo;
+    }
+}
